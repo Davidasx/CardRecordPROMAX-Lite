15f5f0d5ee460a810273cfa05956fc50c1642b43
Aoshi Xu <196603834+Davidasx@users.noreply.github.com>
2025-07-25 22:19:50 +0800

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         CardRecordPROMAX-Lite
+// @namespace    http://tampermonkey.net/
+// @description  CardRecordPROMAX Lite
+// @author       Several People
+// @match        *://ruarua.ru/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.style.userSelect = "auto";
+})();
